0f5002517b228bd3052aaed5121658d06da77fc4
Shrestha <93245080+Shresthaa24@users.noreply.github.com>
2022-11-27 00:24:45 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int largest(int list[], int n);
+int main()
+{
+    int n, i;
+    int list[n];
+    printf("enter size:\n");
+    scanf("%d", &n);
+    printf("the elements in array of %d :\n", n);
+    scanf("%d", &list[n]);
+    int l = largest(list, n);
+    printf("the largest number in the %d array is %d", n, l);
+    return 0;
+}
+int largest(int list[], int n)
+{
+    int i;
+    int l = list[0];
+    // loop
+    for (i = 1; i < n; i++)
+
+        if (list[i] > l)
+            l = list[i];
+    return 1;
+}
